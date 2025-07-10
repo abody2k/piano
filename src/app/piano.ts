1164d9fbdac91241ec3af1ab40d9c5b43d94be02
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2025-07-10 20:54:30 +0300

    set up the project and added the piano empty files

---
+++ src/app/piano.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector:"piano",
+    template:`
+    
+    <div class="flex flex-row w-screen"> </div>
+    
+    `
+})
+export class Piano {
+    constructor() {
+        
+    }
+}
